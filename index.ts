7abd45c3cc8f06cba3c8b6e6b532c50fc061830c
AhmadAbdelsalam <ahmedmohamed11782001@gmail.com>
2023-06-15 02:01:36 +0200

    last

--- index.ts
+++ index.ts
@@ -9,7 +9,9 @@ const app: Express = express();
 
 app.use(express.json());
 app.use("/user", userRouter);
-
+app.get("/", (req: Request, res: Response) => {
+     res.send("api main page");
+});
 app.listen(PORT, () => {
      console.log(`the app is listening on ${PORT}`);
 });
